a4c88c04d68df69539b3d671aafc87d9f21fccec
ahmedbashir-dev <ahmedbashir.w@gmail.com>
2025-04-18 01:59:26 +0500

    Added method to find task by status

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,12 +1,16 @@
 package com.example.taskmanager.repository;
 
+import com.example.taskmanager.enums.TaskStatus;
 import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID> {
+public interface TaskRepository extends JpaRepository<Task, UUID>{
 
+    // Method to find tasks by their status
+    List<Task> findByStatus(TaskStatus status);
 }
