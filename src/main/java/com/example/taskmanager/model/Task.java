10e6b7ff83e167e7a33eebd6a27b81dfe6ae3714
ahmedbashir-dev <ahmedbashir.w@gmail.com>
2025-04-18 01:56:15 +0500

    Task Status Enum Added

--- src/main/java/com/example/taskmanager/model/Task.java
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -1,4 +1,28 @@
 package com.example.taskmanager.model;
 
+import com.example.taskmanager.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+
 }
