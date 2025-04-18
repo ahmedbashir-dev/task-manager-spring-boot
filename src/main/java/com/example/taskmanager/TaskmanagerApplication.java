bf1fe89ad750a547442746ad7ccd781c8c752366
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-04-19 03:59:58 +0500

    Removed property value - debug

--- src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -1,13 +1,19 @@
 package com.example.taskmanager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskmanagerApplication {
+public class TaskmanagerApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
+
 
 }
